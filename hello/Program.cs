bc12a785941e3bcce40c18315fc69a01108b665a
arthurw007BSU <arthur.williamson@bsu.edu>
2025-01-26 17:43:57 +0000

    We ask for a number and display it

--- hello/Program.cs
+++ hello/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Enter a number: ");
+        var number = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("this is your number: "+number );
+
     }
 }
